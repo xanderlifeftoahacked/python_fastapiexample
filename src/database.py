c0d5c14961c935ad2d1a1b41e4dda5268b97334c
xanderlifeftoahacked <freztshow@gmail.com>
2024-03-13 22:32:13 +0300

    probe

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from typing import Optional
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-engine = create_async_engine('sqlite+aiosqlite:///tasks.db')
+engine = create_async_engine('sqlite+aiosqlite:///../tasks.db')
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
